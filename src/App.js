37fa7b6cbce42892f43238948e9f66f48139ba44
ssyeon612 <ssyeon612@gmai.com>
2022-06-05 17:42:54 +0900

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// /* eslint-disable */
+
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [title, chgTitle] = useState(["c", "g", "a"]);
+    let [like, likeCnt] = useState(0);
+
+    return (
+        <div className="App">
+            <div className="black-nav">
+                <h4>React Blog</h4>
+            </div>
+            <div className="list">
+                <h4>
+                    {title[0]}
+                    <span onClick={() => likeCnt(like++)} style={{ cursor: "pointer" }}>
+                        &nbsp; 👍&nbsp;
+                    </span>
+                    {like} &nbsp;
+                </h4>
+                <p>post on 1th June</p>
+            </div>
+
+            <div className="list">
+                <h4>{title[1]}</h4>
+                <p>post on 2th June</p>
+            </div>
+            <div className="list">
+                <h4>{title[2]}</h4>
+                <p>post on 2th June</p>
+            </div>
+
+            <Modal></Modal>
+        </div>
+    );
 }
 
+function Modal() {
+    return (
+        <div className="modal">
+            <h4>title</h4>
+            <p>date</p>
+            <p>contents</p>
+        </div>
+    );
+}
 export default App;
